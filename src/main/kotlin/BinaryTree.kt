ad5d72e9693dfdbbf502c7b157750b111d1eb795
Zachary Anderson <zanderson3j@gmail.com>
2018-05-07 19:44:56 -0400

    refactoring

--- src/main/kotlin/BinaryTree.kt
+++ src/main/kotlin/BinaryTree.kt
@@ -1,5 +1,3 @@
-
-
 class BinaryTree {
     private var root: Node
 
@@ -7,32 +5,20 @@ class BinaryTree {
         root = Node(rootVal)
     }
 
-    fun insert(value: Int) {
-        insertRecursive(root, value)
+    fun insert(value: Int) = insertRecursive(root, value)
+
+    private fun insertRecursive(node: Node, value: Int) {
+        if (value < node.value)
+            if (node.left == null) node.left = Node(value)
+            else insertRecursive(node.left!!, value)
+        else
+            if (node.right == null) node.right = Node(value)
+            else insertRecursive(node.right!!, value)
     }
 
-    fun insertRecursive(node: Node, value: Int) {
-        if(value < node.value) {
-            if(node.left == null) {
-                node.left = Node(value)
-            } else {
-                insertRecursive(node.left!!, value)
-            }
-        } else {
-            if(node.right == null) {
-                node.right = Node(value)
-            } else {
-                insertRecursive(node.right!!, value)
-            }
-        }
-        return
-    }
+    fun depthFirstSearch(value: Int) = dfsRecursive(root, value)
 
-    fun depthFirstSearch(value: Int): Boolean {
-        return dfsRecursive(root, value)
-    }
-
-    fun dfsRecursive(node: Node?, value: Int): Boolean {
+    private fun dfsRecursive(node: Node?, value: Int): Boolean {
         if(node == null) return false
         if(node.value == value) return true
         if(dfsRecursive(node.left, value)) return true
@@ -40,21 +26,15 @@ class BinaryTree {
         return false
     }
 
-    fun inOrderTraversal(): List<Int> {
-        return iotRecursive(root)
-    }
+    fun inOrderTraversal() = iotRecursive(root)
 
-    fun iotRecursive(node: Node?): List<Int> {
-        if(node == null) return emptyList()
-        
-        return iotRecursive(node.left)
+    private fun iotRecursive(node: Node?): List<Int> =
+        if(node == null) emptyList()
+        else iotRecursive(node.left)
                 .union(listOf(node.value))
                 .union(iotRecursive(node.right)
                 .toList())
                 .toList()
-    }
-
-
 }
 
 data class Node(var value: Int) {
